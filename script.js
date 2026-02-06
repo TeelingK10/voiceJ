3ab564695c51484f0d25d74191396151b7da60a5
TeelingK10 <kaitomar0706@gmail.com>
2026-02-06 17:44:04 +0900

    Change fetch URL and improve response handling
    Updated the fetch URL and response handling in the form submission logic.

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 const form = document.getElementById("form");
 
-form.addEventListener("submit", e => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const data = Object.fromEntries(
-    new FormData(form)
-  );
+  const data = Object.fromEntries(new FormData(form));
 
-  fetch("https://docs.google.com/spreadsheets/d/1auqhm91n7pQWOXhfqIQSq2qlxe1K4sKZcQd--6DmHfg/edit?usp=sharing", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  })
-  .then(res => {
-    if (!res.ok) throw new Error();
-    alert("送信が完了しました");
+  fetch(
+    "https://script.google.com/macros/s/AKfycbzpX204tYylSZU37-FQ6tECw2725ha-WieieVaDMZ6JukIQTtFymo4cQZ9nsqeKARTs/exec",
+    {
+      method: "POST",
+      body: JSON.stringify(data)
+    }
+  )
+  .then(res => res.text())
+  .then(text => {
+    alert("送信完了");
     form.reset();
   })
-  .catch(() => {
+  .catch(err => {
+    console.error(err);
     alert("送信に失敗しました");
   });
 });
